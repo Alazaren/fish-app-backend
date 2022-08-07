a1bfdd296dea905923650278f44d7b12fad216dc
andrii lazarenko <alazaren87@gmail.com>
2022-08-07 17:28:38 +0300

    fix typo requirements

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
-from fastai.vision.all import *
+from flask_cors import CORS,cross_origin
+
+from PIL import Image
+import pathlib
+import numpy as np
+
+from fastai.learner import load_learner
+
 from icevision.all import *
 from icevision.models import *
-from numpy import asarray
-from fastai.learner import load_learner
+
+
+
 # from fastai.vision import load_image
-from flask_cors import CORS,cross_origin
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
@@ -15,11 +22,12 @@ learn = load_learner('export.pkl')
 classes = learn.dls.vocab
 
 # load the object detection
-modelPath = Path('Fish_checkpoint.pth')
+modelPath = pathlib.Path('Fish_checkpoint.pth')
 checkpoint_and_model = model_from_checkpoint(modelPath)
-    
+
 
 def predict_single(img_file):
+    img_file = Image.open(img_file)
     model = checkpoint_and_model["model"]
     model_type = checkpoint_and_model["model_type"]
     class_map = checkpoint_and_model["class_map"]
@@ -28,7 +36,7 @@ def predict_single(img_file):
     img_size = checkpoint_and_model["img_size"]
     valid_tfms = tfms.A.Adapter([*tfms.A.resize_and_pad(img_size), tfms.A.Normalize()])
     
-    img_file = load_image(img_file)
+    # img_file = get_image_files(img_file)
 
     preds = model_type.end2end_detect(img_file, valid_tfms, model, class_map=class_map, detection_threshold=0.5)
 
@@ -36,8 +44,12 @@ def predict_single(img_file):
     result = [];
     for bbox in preds['detection']['bboxes']:
         img1 = img_file.crop(bbox.xyxy)
-        numpydata = asarray(img1)
-        prediction = learn.predict(numpydata)
+        
+        # img_byte_arr = io.BytesIO()
+        # img1.save(img_byte_arr, format='PNG')
+        # img_byte_arr = img_byte_arr.getvalue()
+
+        prediction = learn.predict(np.asarray(img1))
         probs_list = prediction[2].numpy()
         result.append(
             {
@@ -52,7 +64,7 @@ def predict_single(img_file):
 # route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    return jsonify(predict_single(request.files['image']))
+    return jsonify(predict_single(request.files['image'].stream))
 
 if __name__ == '__main__':
     app.run()
